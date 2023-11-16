144075690a6e6ccb103bee6ffd5288f48acdaf60
Goh ShuWei <gsw.thc@gmail.com>
2023-11-16 14:49:31 +0800

    convert to app from page

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="min-h-screen">
-      <body className={inter.className}>
+      <body
+        className={
+          inter.className + " max-w-5xl mx-auto px-4 xl:px-0 min-h-screen"
+        }
+      >
         <AuthContext>
           <Nav />
           {children}
